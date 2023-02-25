634f882c75a1facbdc8b2dcfa86d3ada0f1cccca
Eduado Catalán <marvineduardocv12@gmail.com>
2023-02-24 18:50:36 -0600

    Actualizacion de variables de entorno y configuraciones principales

--- Backend/Server Node/src/config.js
+++ Backend/Server Node/src/config.js
@@ -12,6 +12,7 @@ export default {
     s3: {
         region: process.env.S3_REGION,
         accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        bucketName: process.env.S3_BUCKET_NAME
     }
 }
