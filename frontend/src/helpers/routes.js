0f4dfc8c2ed49e8bf81432a5e3af2ca0c7d1cd3e
Adrian <3022643370101@ingenieria.usac.edu.gt>
2023-03-03 13:00:31 -0600

    Update url servidor

--- frontend/src/helpers/routes.js
+++ frontend/src/helpers/routes.js
@@ -1,11 +1,10 @@
-
-export const url_servidor = "http://localhost:3010/api"
+export const url_servidor = "http://lb-practica1-332677503.us-east-2.elb.amazonaws.com/api";
 
 export const URLS = {
-    user:`${url_servidor}/user`,
-    perfil:`${url_servidor}/user/get`,
-    login: `${url_servidor}/user/login`,
-    album: `${url_servidor}/album`,
-    album_get: `${url_servidor}/album/get`,
-    picture: `${url_servidor}/picture`
-}
+  user: `${url_servidor}/user`,
+  perfil: `${url_servidor}/user/get`,
+  login: `${url_servidor}/user/login`,
+  album: `${url_servidor}/album`,
+  album_get: `${url_servidor}/album/get`,
+  picture: `${url_servidor}/picture`,
+};
