41b7f5024fb294c28c29eda71ba8d2115abea9ca
Eduardo Catalán <mcatalan@cartacorp.net>
2023-03-24 10:08:32 -0600

    configuracion directa

--- Backend/Server Node/src/config.js
+++ Backend/Server Node/src/config.js
@@ -3,27 +3,27 @@ import dotenv from "dotenv";
 dotenv.config({path: `./.env.${process.env.NODE_ENV}`});
 export default {
     mysql: {
-        host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        port: process.env.PORT,
-        database: process.env.DATABASE_NAME,
+        host: "database-photobucket.cr1hjnbhot0g.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "seminario1-grupo1",
+        port: "3306",
+        database: "db-photobucket",
         connectionLimit: 10
     },
     s3: {
-        region: process.env.S3_REGION,
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-        bucketName: process.env.S3_BUCKET_NAME
+        region: "us-east-1",
+        accessKeyId: "AKIAV2WSSDG75MMIAJ6P",
+        secretAccessKey: "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",
+        bucketName: "practica1-g1-imagenes-semi1"
     },
     rekognition: {
-        region: process.env.REKOGNITION_REGION,
-        accessKeyId: process.env.REKOGNITION_ACCESS_KEY_ID,
-        secretAccessKey: process.env.REKOGNITION_SECRET_ACCESS_KEY
+        region: "us-east-1",
+        accessKeyId: "AKIAV2WSSDG7SPU3SXXQ",
+        secretAccessKey: "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"
     },
     translate: {
-        region: process.env.TRASLATE_REGION,
-        accessKeyId: process.env.TRASLATE_ACCESS_KEY_ID,
-        secretAccessKey: process.env.TRASLATE_SECRET_ACCESS_KEY
+        region: "us-east-1",
+        accessKeyId: "AKIAV2WSSDG73N6GLK6L",
+        secretAccessKey: "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"
     }
 }
